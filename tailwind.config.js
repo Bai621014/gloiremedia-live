99a2a6ddfb6be7d76dbdd066b4dda3d330196337
Bai621014 <bainimanengandjei@gmail.com>
2026-05-06 10:13:24 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gloire-gold': '#ca8a04',
+        'gloire-dark': '#111111',
+        'gloire-red': '#dc2626',
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+}
